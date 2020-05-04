82cef717fb30528cbc910fdaf8cfd76a1e524960
Darthrabbit <kersken.l@live.de>
2020-05-04 18:16:30 +0200

    Changed get_alphas for more readability
    Pass the step sizes in the directions directltly to not imply how the array should be structured and for more readability

--- Lab1/linear_system.m
+++ Lab1/linear_system.m
@@ -6,9 +6,10 @@ A = sparse(n);
 b = feval(rhs, domain(:,1), domain(:, 2));
 
 for row = 1 : n
-    alpha = get_alphas(stepsizes(row,:));
+    alpha = get_alphas( stepsizes(row,1), stepsizes(row,2), ...
+                        stepsizes(row,3), stepsizes(row,4));
     
-    A(row, row) = alpha(1);    
+    A(row, row) = alpha(1);
     
     for i = 1 : 4
         idx = neighbours(row, i);
@@ -16,7 +17,7 @@ for row = 1 : n
             A(row, idx) = alpha(i+1);
         else
             idx = -idx;
-            b(row) = b(row) - alpha(i+1)*feval(border_func, border(idx, 1), border(idx, 2));
+            b(row) = b(row) - alpha(i+1) * border_func(border(idx, 1), border(idx, 2));
         end
     end
 end
