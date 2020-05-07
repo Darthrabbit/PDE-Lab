89760d58c2b87b2d4b39a3ed808c76e0e015148d
Darthrabbit <kersken.l@live.de>
2020-05-07 14:14:29 +0200

    Update script.m
    Final running version

--- Lab1/script.m
+++ Lab1/script.m
@@ -8,23 +8,24 @@ m = 100;
 [u3, ~] = poisson(@f2, @g1, m);
 [u4, ~] = poisson(@f2, @g2, m);
 
+
 subplot(2, 4, 1)
-s = surf(u1)
+s = surf(u1);
 s.EdgeColor = 'none';
 title("f1, g1")
 
 subplot(2, 4, 2)
-s = surf(u2)
+s = surf(u2);
 s.EdgeColor = 'none';
 title("f1, g2")
 
 subplot(2, 4, 5)
-s = surf(u3)
+s = surf(u3);
 s.EdgeColor = 'none';
 title("f2, g1")
 
 subplot(2, 4, 6)
-s = surf(u4)
+s = surf(u4);
 s.EdgeColor = 'none';
 title("f2, g2")
 
